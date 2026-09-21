8253777ace54590631a7780199e72793151c0d08
Prithvi rathore <prithvir403@gmail.com>
2026-09-21 20:03:06 +0530

    CountDigit

---
+++ BasicMaths/CountDigits.java
@@ -0,0 +1,18 @@
+package BasicMaths;
+
+public class CountDigits {
+  public int countDigit(int n) {
+    int count = 0;
+
+    if (n == 0) {
+      return 1;
+    }
+
+    while (n > 0) {
+      count++;
+      n = n / 10;
+    }
+
+    return count;
+  }
+}
