07ef3fb36fd4891b781650b34fa520394f310fe2
Prithvi rathore <prithvir403@gmail.com>
2026-09-24 16:44:03 +0530

    Add palindrome solution

---
+++ BasicMaths/Palindrome.java
@@ -0,0 +1,16 @@
+package BasicMaths;
+
+public class Palindrome {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+        int temp=x;
+        while(temp>0){
+            int ld=temp%10;
+            rev=(rev*10)+ld;
+            temp=temp/10;
+        }
+        if(x==rev)    return true;
+        else    return false;
+ 
+    }
+}
